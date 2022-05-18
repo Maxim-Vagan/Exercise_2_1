e2802e7473781d4a6e5052f82d830610e8854662
Maxim_Vagan <maxut@gmail.com>
2022-05-18 22:05:34 +0300

    Максим Сергеевич: Решение Заданий 5-8

---
+++ src/ru/maxvagan/Main.java
@@ -0,0 +1,51 @@
+package ru.maxvagan;
+
+import java.util.Arrays;
+
+public class Main {
+
+    private static void changeValue(int value) {
+        value = 22;
+    }
+
+    private static void changeValue2(Integer mainValue) {
+        mainValue = 22;
+    }
+
+    private static void changeArrayValue(Integer[] inpArrayValue) {
+        inpArrayValue = new Integer[]{1, 2};
+    }
+
+    private static void changeArrayValue2(Integer[] inpArrayValue) {
+        inpArrayValue[0] = 99;
+    }
+
+    public static void main(String[] args) {
+	    // task 5
+        System.out.println("Task 5");
+        int value = 33;
+        System.out.println("value before void treatment = " + value);
+        changeValue(value);
+        System.out.println("value after void treatment = " + value);
+        // task 6
+        System.out.println("Task 6");
+        Integer mainValue = 33;
+        System.out.println("mainValue before void treatment = " + mainValue);
+        changeValue2(mainValue);
+        System.out.println("mainValue after void treatment = " + mainValue);
+        // task 7
+        System.out.println("Task 7");
+        Integer[] arrayValue = {3, 4};
+        System.out.println("arrayValue before void treatment = " + Arrays.toString(arrayValue));
+        changeArrayValue(arrayValue);
+        System.out.println("arrayValue after void treatment = " + Arrays.toString(arrayValue));
+        // task 8
+        System.out.println("Task 8");
+        Integer[] arrayValue2 = {3, 4};
+        System.out.println("arrayValue2 before void treatment = " + Arrays.toString(arrayValue2));
+        changeArrayValue2(arrayValue2);
+        System.out.println("arrayValue2 after void treatment = " + Arrays.toString(arrayValue2));
+        // task 9
+        System.out.println("Task 9");
+    }
+}
